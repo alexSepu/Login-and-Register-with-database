e42158bfc2593f4db3fac5a3da88b3ac7fb5e9c3
alexSepu <73934209+alexSepu@users.noreply.github.com>
2021-01-29 19:54:41 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    require_once("lib/connexiopersistent.php");
-    $sql = "UPDATE users set active = ? where iduser = ?";
-    $update = $db -> prepare($sql);
-    $update -> execute(array(1,$_SESSION["id"]));
-    $_SESSION = array();
-    session_destroy();
-    header("Location:index.php");
